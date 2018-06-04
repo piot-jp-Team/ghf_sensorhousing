1bb680de21f19d5f02ca5de5b5faac3042e8061f
kojiyoshida <kojiyoshida@199.34.57.15>
2018-06-04 17:29:43 +0900

    ghf_sakura_io

---
+++ FIRMWARE/ghf_sakura_io/ghf_sakura_io.ino
@@ -0,0 +1,65 @@
+
+#include <SakuraIO.h>
+
+// Sakura IO
+SakuraIO_I2C sakuraio;
+
+void setup() {
+
+  // オンラインになるまで待つ
+  while ((sakuraio.getConnectionStatus() & 0x80) != 0x80) {
+    delay(1000);
+  }
+
+
+}
+
+void loop() {
+
+  static uint32_t i;
+  uint8_t j, result;
+  uint16_t data[6];
+  
+  i++;
+
+  
+  // 受信キューの状態を取得
+  uint8_t rxAvailable;
+  uint8_t rxQueued;
+  sakuraio.getRxQueueLength(&rxAvailable, &rxQueued);
+
+  // 受信キューにたまっているメッセージの数だけ繰り返す
+  for (uint8_t i = 0; i < rxQueued; i++) {
+    uint8_t channel;
+    uint8_t type;
+    uint8_t values[8];
+    int64_t offset;
+
+    // キューからのメッセージを取り出しに成功したら以下を実行
+    uint8_t ret = sakuraio.dequeueRx(&channel, &type, values,
+                                     &offset);
+    if (ret == CMD_ERROR_NONE) {
+/*
+      Serial.print("channel: ");
+      Serial.println(channel);
+      Serial.print("type: ");
+      Serial.println((char)type);
+      Serial.print("value: ");
+*/
+      //0x03 - Read Holding Registers (channel:"1", type:i ,value:)
+      if (type == 'i') {
+        int32_t value = 0;
+        memcpy(&value, &values, sizeof(int32_t));
+
+      }
+
+    } else {
+      //sakuraio.dequeueRx ERROR
+      delay(5000);
+    }
+  }
+
+
+}
+
+
---
+++ FIRMWARE/ghfsensor/ghfsensor.ino
@@ -0,0 +1,63 @@
+#include <Adafruit_Sensor.h>
+#include <Adafruit_AM2320.h>
+#include <MHZ19_uart.h>
+#include <ModbusRtu.h>
+
+const int rx_pin = 4;  //Serial rx pin no
+const int tx_pin = 5; //Serial tx pin no
+Adafruit_AM2320 am2320 = Adafruit_AM2320();
+MHZ19_uart mhz19;
+int slaveID = 1;
+Modbus slave(slaveID, 0, 0); // this is slave ID ,serial pin(0=default), tx_enable_pin(0=none)
+// data array for modbus network sharing
+uint16_t au16data[6]; 
+
+void setup() {
+  Serial.begin(9600);
+  
+  am2320.begin();
+  mhz19.begin(rx_pin, tx_pin);
+  mhz19.setAutoCalibration(false);
+  while( mhz19.isWarming() ) {
+    Serial.print("MH-Z19 now warming up...  status:");Serial.println(mhz19.getStatus());
+    delay(1000);
+  }
+
+//自分のアドレス取得
+//(D9 D8 D7)
+bitWrite( slaveID, 0, digitalRead( 9 ));
+bitWrite( slaveID, 1, digitalRead( 8 ));
+bitWrite( slaveID, 2, digitalRead( 7 ));
+slave.setID(slaveID);
+
+}
+
+void loop() {
+  
+  int temp2=am2320.readTemperature();
+  int hum=am2320.readHumidity();
+  int co2ppm = mhz19.getPPM();
+  int temp = mhz19.getTemperature();
+  //Serial.print("co2: "); Serial.println(co2ppm);
+  char PPMX[2];
+  sprintf(PPMX,"%X", co2ppm);
+  Serial.print(PPMX);
+  //Serial.print("temp: "); Serial.println(temp);
+  char TEMPX[2];
+  sprintf(TEMPX,"%X", temp);
+  Serial.print(TEMPX);
+  char HUMX[2];
+  sprintf(HUMX,"%X", hum);
+  Serial.print(HUMX);
+
+  delay(5000);
+ 
+
+
+//リクエスト待機
+//自分のアドレス一致したら応答
+//ToDo
+
+
+
+}
